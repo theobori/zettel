97be1bec5f6bb5397e15e119bed3846bc5c221c2
Théo Bori <theo1.bori@epitech.eu>
2025-02-23 01:35:37 +0100

    :sparkles: Initial zettel with Nix build and Github Action workflow

---
+++ default.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  stdenvNoCC,
+  emanote,
+}:
+stdenvNoCC.mkDerivation {
+  pname = "notes";
+  version = "0.0.1";
+
+  src = ./.;
+
+  nativeBuildInputs = [
+    emanote
+  ];
+
+  env = {
+    LC_ALL = "C.UTF-8";
+  };
+
+  buildPhase = ''
+    runHook preBuild
+
+    mkdir -p output
+    emanote --layers "zettel" gen output
+
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook predInstalll
+
+    # mkdir -p $out
+    cp -r output $out
+
+    runHook postInstalll
+  '';
+
+  meta = {
+    description = "";
+    homepage = "";
+    license = lib.licenses.mit;
+  };
+}
